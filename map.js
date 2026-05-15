efaec68c7c11d867d4f026b8c4a1af4b9e9100b2
Yall777 <75231012+Yall777@users.noreply.github.com>
2026-05-15 15:53:56 -0700

    edit

--- map.js
+++ map.js
@@ -10,9 +10,41 @@ mapboxgl.accessToken = accessToken;
 // Initialize the map
 const map = new mapboxgl.Map({
     container: 'map',
-    style: 'mapbox://styles/mapbox/light-v11',
+    style: 'mapbox://styles/yall777/cmp7hv8m7000o01sm0d8qhp3e',
     center: [-71.09415, 42.36027],
     zoom: 12,
     minZoom: 5,
     maxZoom: 18,
+});
+
+map.on('load', async () => {
+    const bikeLanePaint = {
+        'line-color': '#32D400',
+        'line-width': 5,
+        'line-opacity': 0.6,
+    };
+
+    map.addSource('boston_route', {
+        type: 'geojson',
+        data: 'https://bostonopendata-boston.opendata.arcgis.com/datasets/boston::existing-bike-network-2022.geojson',
+    });
+
+    map.addLayer({
+        id: 'bike-lanes-boston',
+        type: 'line',
+        source: 'boston_route',
+        paint: bikeLanePaint,
+    });
+
+    map.addSource('cambridge_route', {
+        type: 'geojson',
+        data: 'https://raw.githubusercontent.com/cambridgegis/cambridgegis_data/main/Recreation/Bike_Facilities/RECREATION_BikeFacilities.geojson',
+    });
+
+    map.addLayer({
+        id: 'bike-lanes-cambridge',
+        type: 'line',
+        source: 'cambridge_route',
+        paint: bikeLanePaint,
+    });
 });
